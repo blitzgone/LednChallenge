ee318314e38c5a11c6325ae3cad9939704de7723
David Sheppard <dave.sheppard@accreon.com>
2021-03-28 22:41:55 -0300

    Tables up and ready for server sort

--- app/src/components/UserTable.jsx
+++ app/src/components/UserTable.jsx
@@ -3,18 +3,21 @@ import axios from 'axios';
 import DataTable from 'react-data-table-component';
 import DataTableExtensions from 'react-data-table-component-extensions';
 import 'react-data-table-component-extensions/dist/index.css';
+
 function UserTable() {
-    const [userDate, setUserDate] = useState(0);
+    const [tableData, setTableDate] = useState({});
   
     useEffect(() => {
-      
       axios.get('http://localhost:3030/users')
       .then(res => {
         const data = res.data;
-        setUserDate({ data });
+        setTableDate ( {
+          columns,
+          data,
+        } );
       })
           
-    });
+    }, []);
 
     const columns = [
         {
@@ -28,6 +31,17 @@ function UserTable() {
           sortable: true,
         },
         {
+          name: 'Tokens',
+          selector: 'amt',
+          sortable: true,
+          cell: row => {
+              return (
+                <div className="table-token">µ <span className="table-token-value">{ new Intl.NumberFormat().format(row.amt)}</span></div>
+              );
+          },
+        },
+        
+        {
           name: 'Country',
           selector: 'Country',
           sortable: true,
@@ -37,26 +51,56 @@ function UserTable() {
           selector: 'email',
           sortable: true,
         },
+        {
+          name: 'Date Of Birth',
+          selector: 'dob',
+          sortable: true,
+        },
+        {
+          name: 'Auth Type',
+          selector: 'mfa',
+          sortable: true,
+          cell: row => {
+            if (row.mfa === 'null') {
+              return (
+                <div></div>
+              );
+            } else {
+              return (
+                <div>{row.mfa}</div>
+              );
+            }
+          },
+        },
+        {
+          name: 'CreatedDate',
+          selector: 'Created',
+          sortable: true,
+        },
+        {
+          name: 'Referred By',
+          selector: 'ReferredBy',
+          sortable: true,
+        },
     ];
 
-  const tableData = {
-    columns,
-    userData,
-  };
+  
   
 
   return (
-    <DataTableExtensions
-      {...tableData}
-    >
-      <DataTable
-        noHeader
-        defaultSortField="id"
-        defaultSortAsc={false}
-        pagination
-        highlightOnHover
-      />
-    </DataTableExtensions>
+    <div>
+      <DataTableExtensions
+        {...tableData}
+      >
+        <DataTable
+          noHeader
+          defaultSortField="id"
+          defaultSortAsc={false}
+          pagination
+          highlightOnHover
+        />
+      </DataTableExtensions>
+    </div>
   );
 }
 
